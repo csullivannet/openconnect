cbdebeb518141ce637b8efc9fcba6d1094bd2341
Christopher Sullivan <christopher.sullivan@razorfish.com>
2017-09-26 21:34:27 -0400

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/ash
-printf "${USER}\n${PASSWORD}\n${TOKEN}\n" | /usr/local/sbin/openconnect --config=/etc/openconnect/opeconnect.conf $@
+printf "${USER}\n${PASSWORD}\n${TOKEN}\n" | /usr/local/sbin/openconnect --config=/etc/openconnect/openconnect.conf $@
